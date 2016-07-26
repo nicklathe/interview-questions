6dd22d133328a561ccca655d1a6afa001fb5677c
nicklathe <nicklathe@icloud.com>
2016-07-26 10:50:00 -0700

    Removed hint about making it functional

--- animals.js
+++ animals.js
@@ -9,6 +9,6 @@ const animals = [
     {name: 'Frank the Tank', species: 'dog'}
 ];
 
-// Using functional Javascript, iterate through this array of animals and filter
+// Iterate through this array of animals and filter
 // out only the dogs, outputting their name and species like this:
 // ['Spot is a dog', 'Django is a dog', 'Frank the Tank is a dog']
